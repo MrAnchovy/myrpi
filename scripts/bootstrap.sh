cea87a7b4c14d5f55c653d0edf51a73ef4919064
MrAnchovy <mr.anchovy@mranchovy.com>
2012-07-09 22:05:54 +0100

    Make tarball extraction work

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-rm -rf /var/myrpi
-mkdir /var/myrpi
+sudo rm -rf /var/myrpi
+sudo mkdir /var/myrpi
 cd /var/myrpi
 wget --no-check-certificate -q https://github.com/MrAnchovy/myrpi/tarball/master
-# tar -xzvf
+tar -xzvf master
+
+if [[ "`ls`" =~ (MrAnchovy-myrpi-[A-Za-z0-9_-]*) ]]; then
+	mv "${BASH_REMATCH[1]}" myrpi
+	cd myrpi
+	./install.sh
+else
+	echo "Error in download - please try again"
+fi
