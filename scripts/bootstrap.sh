65e23ab4a51df3e2d508ab3886c87692446363b8
MrAnchovy <mr.anchovy@mranchovy.com>
2012-07-09 22:28:16 +0100

    Add directory changes, noauto option

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
 ME=`whoami`
+PWD=`pwd`
+ARG1=$1
+ARG2=$2
+
+echo "------------------------------------------------------------"
+echo "Cleaning up any old download and preparing for a new one"
+echo "[ARG1][$ARG2]"
 
 sudo rm -rf /var/myrpi
 sudo mkdir /var/myrpi
 sudo chown $ME /var/myrpi
 cd /var/myrpi
+
+echo "------------------------------------------------------------"
+echo "Downloading installation script
+
 wget --no-check-certificate -q https://github.com/MrAnchovy/myrpi/tarball/master
+
+echo "------------------------------------------------------------"
+echo "Extracting installation script
+echo "------------------------------------------------------------"
+
 tar -xzvf master
 
 if [[ "`ls`" =~ (MrAnchovy-myrpi-[A-Za-z0-9_-]*) ]]; then
 	mv "${BASH_REMATCH[1]}" myrpi
+
 	cd myrpi
 	./install.sh
+	cd $PWD
 else
 	echo "Error in download - please try again"
 fi
