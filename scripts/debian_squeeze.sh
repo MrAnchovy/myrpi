2a100129252e6509b8e3734fed780197211cd385
Mr Anchovy <mr.anchovy@mranchovy.com>
2012-07-12 22:02:09 +0100

    Working version, corrected unix line endings

--- scripts/debian_squeeze.sh
+++ scripts/debian_squeeze.sh
@@ -2,10 +2,26 @@
 
 LOG=$1
 
+# UTILITY FUNCTIONS ===========================================================
+
 function fn_log_echo {
 	echo $1
 	echo `date +"%Y-%m-%d %T"` $1 >> $LOG
 }
+
+function fn_append {
+	echo $1 | sudo tee -a $2 > /dev/null
+}
+
+function fn_apt_add_key {
+	wget -O - -q $1 | sudo apt-key add -
+}
+
+function fn_apt_add_source {
+	fn_append "\n$1\n" /etc/apt/sources.list
+}
+
+# MAIN FUNCTIONS ==============================================================
 
 function fn_apt_update {
 	echo "------------------------------------------------------------"
@@ -47,6 +63,7 @@ function fn_nginx_install {
 		fn_log_echo "FAILED: could not install nginx - exiting"
 		exit 1
 	fi
+
 }
 
 function fn_nginx_reinstall {
@@ -59,40 +76,97 @@ function fn_nginx_reinstall {
 		fn_log_echo "FAILED: could not re-install nginx - exiting"
 		exit 1
 	fi
-}
 
-function fn_append {
-	echo $1 | sudo tee -a $2 > /dev/null
-}
-
-function fn_apt_add_key {
-	wget -O - -q $1 | sudo apt-key add -
-}
-
-function fn_apt_add_source {
-	fn_append "\n$1\n" /etc/apt/sources.list
 }
 
 
-function fn_php_install() {
+function fn_nginx_conf {
+	echo
+}
 
-	echo    "------------------------------------------------------------"
-	echo    "Installing php5"
+function fn_nginx_vhost {
 
-	INSTALL="php5-fpm"
-	sudo apt-get install $INSTALL
+	## disable any existing default vhosts
+	sudo rm /etc/nginx/sites-enabled/default*
 
-	INSTALL="php5-cgi"
-	INSTALL="$INSTALL php5-mysql php5-curl php5-gd php5-idn php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl"
-	INSTALL="$INSTALL php-pear"
-	sudo apt-get install $INSTALL
+	## copy the vhost templates into place and link the correct one
+	sudo cp ../templates/default-no-php /etc/nginx/sites-available
+	sudo cp ../templates/default-php /etc/nginx/sites-available
+	sudo ln -s /etc/nginx/sites-available/default-no-php /etc/nginx/sites-enabled/default-no-php
 
-	if [ "$?" != "0" ]; then
-		echo "------------------------------------------------------------"
-		echo "FAILED: could not install php5 - exiting"
-		exit 1
+	#sudo chown root:root local-default
+
+	sudo mkdir -p /var/www/default/public_html
+	sudo mkdir -p /var/www/default/log
+
+	sudo nginx -s reload
+}
+
+function fn_php_check {
+	if [[ `whereis php` =~ /usr/bin/php ]]; then
+		PHP_IS_INSTALLED=1
+	else
+		PHP_IS_INSTALLED=0
 	fi
 }
+
+function fn_php_install {
+
+	fn_log_echo "Installing PHP"
+
+	sudo apt-get install php5-cgi 2>> LOG
+
+	if [ "$?" != "0" ]; then
+		fn_log_echo "FAILED: could not install PHP - exiting"
+		exit 1
+	fi
+
+}
+
+function fn_php_reinstall {
+
+	fn_log_echo "Re-installing PHP"
+
+	sudo apt-get install --reinstall php5-cgi 2>> LOG
+
+	if [ "$?" != "0" ]; then
+		fn_log_echo "FAILED: could not re-install PHP - exiting"
+		exit 1
+	fi
+
+}
+
+
+function fn_php_conf {
+	echo
+}
+
+function fn_php_fcgi {
+	
+	## disable any existing default vhosts
+	sudo rm /etc/nginx/sites-enabled/default*
+
+	## copy the vhost templates into place and link the correct one
+	sudo cp ../templates/default-php /etc/nginx/sites-available
+	sudo ln -s /etc/nginx/sites-available/default-php /etc/nginx/sites-enabled/default-php
+
+	sudo nginx -s reload
+
+	sudo cp ../templates/php-fastcgi /etc/init.d/php-fastcgi
+	# make it executable
+	sudo chmod +x /etc/init.d/php-fastcgi
+	# start it now
+	sudo invoke-rc.d php-fastcgi start
+	# and finally make it permanent
+	sudo insserv php-fastcgi
+}
+
+
+
+
+
+
+
 
 function mysql_install() {
 
@@ -112,95 +186,6 @@ function mysql_conf {
 	echo
 }
 
-function fn_nginx_conf {
-	echo
-}
-
-function fn_nginx_vhost {
-
-	## create the PHP part of the script, commenting it out if not needed
-	if [ "$PHP" != "0" ]; then
-
-		PHP_TEXT="
-	# pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
-	#
-	#{if php}
-	location ~ \.php$ {
-		try_files \$uri =404;
-		fastcgi_pass 127.0.0.1:9000;
-		fastcgi_index index.php;
-		fastcgi_param  SCRIPT_FILENAME  /var/www\$fastcgi_script_name;
-		include fastcgi_params;
-	}"
-	#{endif php}
-
-	else
-		PHP_TEXT="
-	#{if php}
-	# pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
-	#
-	# location ~ \.php$ {
-	#	try_files \$uri =404;
-	#	fastcgi_pass 127.0.0.1:9000;
-	#	fastcgi_index index.php;
-	#	fastcgi_param  SCRIPT_FILENAME  /var/www\$fastcgi_script_name;
-	#	include fastcgi_params;
-	# }"
-	#{endif php}
-
-	
-	fi
-
-	CONTENT="
-server {
-        listen   80; ## listen for ipv4; this line is default and implied
-        # {{listen   [::]:80 default ipv6only=on; ## listen for ipv6}}
-
-	# Make site accessible from http://localhost/
-        server_name localhost;
-
-	location / {
-                root        /var/www/default/public_html;
-	        access_log  /var/www/default/log/access.log;
-	        error_log   /var/www/default/log/error.log;
-
-		# First attempt to serve request as file, then
-                # as directory, then fall back to index.html
-                try_files $uri $uri/ /index.html /index.php /index.htm;
-        }
-
-	# error_page 404 /404.html;
-
-	# redirect server error pages to the static page /50x.html
-        #
-        error_page 500 502 503 504 /50x.html;
-        location = /50x.html {
-                root /usr/share/nginx/www;
-        }
-
-	$PHP_TEXT
-
-	# deny access to .htaccess files, if Apache's document root
-        # concurs with nginx's one
-        #
-        location ~ /\.ht {
-                deny all;
-        }
-}"
-
-	## disable the existing default vhost
-	if [ -f /etc/nginx/sites-available/default ]; then
-		sudo unlink /etc/nginx/sites-available/default
-	fi
-
-	## create a new vhost file, move it into place and link it
-	echo "$CONTENT" > local-default
-	sudo chown root:root local-default
-	sudo mv local-default /etc/nginx/sites-available/
-	sudo ln -sf /etc/nginx/sites-available/local-default /etc/nginx/sites-enabled/local-default
-	mkdir -p /var/www/default/public_html
-	mkdir -p /var/www/default/log
-}
 
 
 function fn_http_index {
@@ -228,7 +213,7 @@ function fn_http_index {
 }
 
 
-
+# CODE STARTS HERE ============================================================
 
 PHP_IS_INSTALLED=0
 MYSQL_IS_INSTALLED=0
@@ -248,18 +233,81 @@ fn_nginx_check
 
 if [ "$NGINX_IS_INSTALLED" = 1 ]; then
 	echo "------------------------------------------------------------"
-	echo -n "Do you want to re-install NGINX (enter Y to re-install) ? "
+	echo -n "Do you want to re-install the nginx web server (enter Y to re-install) ? "
 	read ANSWER
-	if [ "$ANSWER" = "Y" ]; then
+	if [[ "$ANSWER" =~ [yY]+ ]]; then
 		fn_nginx_reinstall
+		fn_nginx_conf
+		fn_nginx_vhost
+		fn_log_echo "nginx re-install complete"
 	else
 		fn_log_echo "nginx re-install skipped"
 	fi
 else
+	echo "------------------------------------------------------------"
 	fn_nginx_install
+	fn_nginx_conf
+	fn_nginx_vhost
+	NGINX_IS_INSTALLED=1
+	fn_log_echo "nginx install complete"
 fi
 
+
+# Deal with PHP   =============================================================
+
+PHP_IS_INSTALLED=0
+
+fn_php_check
+
+if [ "$PHP_IS_INSTALLED" = 1 ]; then
+	echo "------------------------------------------------------------"
+	echo -n "Do you want to re-install PHP (enter Y to re-install) ? "
+	read ANSWER
+	if [[ "$ANSWER" =~ [yY]+ ]]; then
+		fn_php_reinstall
+		fn_php_conf
+		fn_php_fcgi
+		fn_log_echo "PHP re-install complete"
+	else
+		fn_log_echo "PHP re-install skipped"
+	fi
+else
+	echo "------------------------------------------------------------"
+	fn_php_install
+	fn_php_conf
+	fn_php_fcgi
+	PHP_IS_INSTALLED=1
+	fn_log_echo "PHP install complete"
+fi
+
+
+# End =========================================================================
+
 exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 echo -n "Do you want to install N"
 read name
