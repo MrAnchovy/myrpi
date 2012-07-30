59a031b000a2562dded7acee84e0541fbf0c3f86
MrAnchovy <mr.anchovy@mranchovy.com>
2012-07-30 22:57:53 +0100

    Add comments to download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -6,28 +6,36 @@ PWD=`pwd`
 echo "------------------------------------------------------------"
 echo "Cleaning up any old download and preparing for a new one"
 
+## Remove completely any previous download
 sudo rm -rf /var/myrpi
+
+## Create directory for download, give it to me and switch to it
 sudo mkdir /var/myrpi
 sudo chown $ME /var/myrpi
 cd /var/myrpi
 
 echo "------------------------------------------------------------"
-echo "Downloading installation script"
+echo "Downloading MyRPi.net installation package"
 
-wget --no-check-certificate -q https://github.com/MrAnchovy/myrpi/tarball/master
+wget --no-check-certificate https://github.com/MrAnchovy/myrpi/tarball/master
 
 echo "------------------------------------------------------------"
-echo "Extracting installation script"
+echo "Extracting MyRPi.net installation package"
 echo "------------------------------------------------------------"
 
+## Unpack downlaod
 tar -xzvf master
 
+## Check download package exists
 if [[ "`ls`" =~ (MrAnchovy-myrpi-[A-Za-z0-9_-]*) ]]; then
+	## Rename download package (contains version number)
 	mv "${BASH_REMATCH[1]}" myrpi
 
-	cd myrpi/scripts
-	chmod +x *
-	cd $PWD
+	## make all scripts executable
+	chmod +x myrpi/scripts/*
+	exit 0
+
 else
 	echo "Error in download - please try again"
+	exit 1
 fi
