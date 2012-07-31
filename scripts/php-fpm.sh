9eff3ada9079a988838bdf3e590911f61b6f04a3
MrAnchovy <mr.anchovy@mranchovy.com>
2012-07-31 04:05:10 +0100

    Improvements to demo templates and sqlite configuration

--- scripts/php-fpm.sh
+++ scripts/php-fpm.sh
@@ -31,12 +31,17 @@ sudo chown $WWW_USER /var/hosts/default/var
 mkdir -p /var/hosts/default/tmp
 sudo chown $WWW_USER /var/hosts/default/tmp
 
+## SQLite directory
+##
+mkdir -p /var/hosts/default/var/sqlite
+sudo chown $WWW_USER /var/hosts/default/var/sqlite
+
 ## Create the public html directory and populate it with demo files. This will
 ## be owned by the current user so will not be writeable by web scripts.
 ##
 mkdir -p /var/hosts/default/public_html
 cp $TEMPLATES_PATH/public_html/* /var/hosts/default/public_html
 
-sudo nginx -s reload
+# sudo nginx -s reload
 
 exit 0
--- templates/public_html/index.php
+++ templates/public_html/index.php
@@ -1,1 +1,28 @@
-<?php phpinfo();
+<?php
+
+error_reporting(-1);
+ini_set('display_errors', 1);
+ini_set('cgi.fix_pathinfo', 0);
+
+date_default_timezone_set('Europe/London');
+
+echo "<pre>\n";
+echo `cat proc/cpuinfo`;
+echo "</pre>\n";
+
+// test SQLite
+$dbfile = '/var/hosts/default/var/sqlite/' . md5(microtime());
+$db = new PDO("sqlite:$dbfile");
+
+$rs = $db->query('create table test ( value )');
+
+if ( $rs===FALSE ) {
+  print_r($db->errorInfo());
+} else {
+  echo 'SQLite OK';
+}
+
+unlink($dbfile);
+
+phpinfo();
+
